96261262208eeb764961afb262275a31bceb7497
Priyanshu30 <priyanshubarapatre30@gmail.com>
2026-07-25 08:01:00 +0530

    Added new pipelines

--- jobdsl/applications.groovy
+++ jobdsl/applications.groovy
@@ -1,24 +1,24 @@
-pipelineJob('Applications/Seed') {
+// pipelineJob('Applications/Seed') {
 
-    description('Regenerates all Jenkins jobs')
+//     description('Regenerates all Jenkins jobs')
 
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/priyanshu-devops-release/jenkins-config-management.git')
-                        credentials('github-token')
-                    }
-                    branch('master')
-                }
-            }
-            scriptPath('Jenkinsfile')
-        }
-    }
+//     definition {
+//         cpsScm {
+//             scm {
+//                 git {
+//                     remote {
+//                         url('https://github.com/priyanshu-devops-release/jenkins-config-management.git')
+//                         credentials('github-token')
+//                     }
+//                     branch('master')
+//                 }
+//             }
+//             scriptPath('Jenkinsfile')
+//         }
+//     }
 
-    logRotator {
-        daysToKeep(30)
-        numToKeep(20)
-    }
-}
+//     logRotator {
+//         daysToKeep(30)
+//         numToKeep(20)
+//     }
+// }
