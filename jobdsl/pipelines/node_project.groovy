4e7f51886ed5f0b9956a7ae05b360b3758f37aaa
Priyanshu30 <priyanshubarapatre30@gmail.com>
2026-07-28 07:04:00 +0530

    granted access for user

--- jobdsl/pipelines/node_project.groovy
+++ jobdsl/pipelines/node_project.groovy
@@ -18,7 +18,7 @@ pipeline {
 
         stage('Checkout') {
             steps {
-                git branch: 'master'
+                git branch: 'master',
                     url: 'https://github.com/priyanshu-devops-release/toolkit.git'
                 // Add credentialsId: 'github-creds' if the repo is private
             }
