c32c977c67fb7977899d7c8bd79c7e5fb8998ee7
Priyanshu30 <priyanshubarapatre30@gmail.com>
2026-07-31 06:22:00 +0530

    Adding new maven build for agetnt testing

--- jobdsl/pipelines/helm_argocd.groovy
+++ jobdsl/pipelines/helm_argocd.groovy
@@ -1,19 +1,64 @@
-pipelineJob('Infrastructure/ArgoCD/Helm-ArgoCD') {
+pipelineJob('Infrastructure/ArgoCD/ArgoCD-Application-Restart') {
 
-    description('Deploy helm-argocd')
+    description('Helm sample pipeline running on Kubernetes kubelet-docker-agent.')
 
     definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/priyanshu-devops-release/helm-argocd.git')
-                        credentials('github-token')
-                    }
-                    branch('master')
+        cps {
+            script("""
+pipeline {
+    agent {
+        label 'kubelet-docker-agent'
+    }
+
+    parameters {
+        choice(
+            name: 'APPLICATION',
+            choices: [
+                'jenkins',
+                'external-secrets',
+                'prometheus',
+                'grafana'
+            ],
+            description: 'Select the application to restart'
+        )
+        string(
+            name: 'NAMESPACE',
+            defaultValue: 'devops-tools',
+            description: 'Kubernetes Namespace'
+        )
+
+        string(
+            name: 'K8s COMPONENT',
+            choices: [
+                'ingress',
+                'service',
+                'deployment'
+            ],
+            description: 'K8sComponent Name'
+        )
+    }
+
+    stages {
+        stage('Restart Application') {
+            steps {
+                container('kubectl') {
+                    sh '''
+                        kubectl rollout restart ${K8s COMPONENT} ${APPLICATION} -n ${NAMESPACE}
+                        kubectl rollout status ${K8s COMPONENT} ${APPLICATION} -n ${NAMESPACE} --timeout=300s
+                    '''
                 }
             }
-            scriptPath('Jenkinsfile')
+        }
+    }
+
+    post {
+        success {
+            echo "Application restarted successfully."
+        }
+    }
+}
+""".stripIndent())
+            sandbox(true)
         }
     }
 }
